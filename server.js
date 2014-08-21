dbfc98568fa6c9c53b523a10f8d0c5afce1edabf
Sherah Smith <ssmith@wikimedia.org>
2014-08-21 16:07:37 -0700

    Add oauth/passportJS
    Needs testing with a soon-to-be-provided drupal connection
    
    Change-Id: Ib41458aa96e78833f66d61bd70f0bfa9cef409d1

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 require('rconsole');
 
-var express     = require( 'express' ),
-    app         = express(),
-    commander   = require( 'commander' ),
-    config      = require( './defaults.js' ),
+var express           = require( 'express' ),
+    app               = express(),
+    commander         = require( 'commander' ),
+    config            = require( './defaults.js' ),
+    passport          = require( 'passport' ),
+    OAuthStrategy     = require( 'passport-oauth' ).OAuthStrategy,
     server,
     serverConfig;
+
+passport.use('provider', new OAuthStrategy({
+    requestTokenURL: 'http://oauth-sandbox.sevengoslings.net/request_token',
+    accessTokenURL: 'http://oauth-sandbox.sevengoslings.net/access_token',
+    userAuthorizationURL: 'http://oauth-sandbox.sevengoslings.net/authorize',
+    consumerKey: '27312d60e9a5eb11',
+    consumerSecret: '2f75ad1bd4abe5c98195cc48bd35'
+  },
+  function(token, tokenSecret, profile, done) {
+    // TODO: this
+    // User.findOrCreate(..., function(err, user) {
+    //   done(err, user);
+    //});
+    console.log('profile: ', profile);
+  }
+));
 
 commander
     .version('0.0.1')
@@ -32,6 +50,11 @@ app.set( 'view options', { pretty: true } );
 app.use( express.bodyParser() );
 app.use( express.methodOverride() );
 app.use( express.static(__dirname + '/src') );
+
+app.get('/auth/provider', passport.authenticate('provider'));
+app.get('/auth/provider/callback',
+  passport.authenticate('provider', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
 
 var port = config.port;
 
