e08cc27d0dc0141a7c69dfc96740847fdf10c12c
Sherah Smith <ssmith@wikimedia.org>
2014-08-25 14:09:53 -0700

    Fix config file madness
    Change-Id: I098a6c9bf86460cae6b5dc24e7e4178e4e53e9ff

--- defaults.js
+++ defaults.js
@@ -2,5 +2,10 @@ module.exports = {
     listen: "8080",
     db: 'fredge',
     dblogin: 'testuser',
-    dbpwd: 'testpassword'
+    dbpwd: 'testpassword',
+    requestTokenURL: 'http://oauth-sandbox.sevengoslings.net/request_token',
+    accessTokenURL: 'http://oauth-sandbox.sevengoslings.net/access_token',
+    userAuthorizationURL: 'http://oauth-sandbox.sevengoslings.net/authorize',
+    consumerKey: '27312d60e9a5eb11',
+    consumerSecret: '2f75ad1bd4abe5c98195cc48bd35'
 };
