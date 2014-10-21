903fe06d38d4780c88088cc91221aa09959633c1
Elliott Eggleston <ejegg@ejegg.com>
2014-10-21 12:54:18 -0700

    Config file doesn't need to export a function
    This patch allows you to use the same format for the real config
    file that you do for the defaults, just exporting an object with
    a bunch of properties.  Any properties existing in defaults.js
    and not in the config file supplied on the command line will be
    copied in from defaults.
    
    Also fix a couple console.err statements
    
    Change-Id: I87e043baa020262668ee3be5108e30f1af202f48

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express           = require( 'express' ),
     app               = express(),
     commander         = require( 'commander' ),
-    config            = require( './defaults.js' ),
-    port              = config.port,
+    defaults          = require( './defaults.js' ),
     routes            = require( './routes'),
     passport          = require( 'passport' ),
     DrupalStrategy    = require( 'passport-drupal' ).DrupalStrategy,
     evilDns			  = require( 'evil-dns' ),
     server,
-    serverConfig;
+    serverConfig,
+    config,
+    prop;
 
 commander
     .version('0.0.1')
@@ -16,17 +17,24 @@ commander
     .parse(process.argv);
 
 try {
-    if (commander.config) {
-        config = require(commander.config)(config);
-    }
+    if ( commander.config ) {
+        config = require( commander.config );
+		for ( prop in defaults ) {
+			if ( defaults.hasOwnProperty( prop ) && !config.hasOwnProperty( prop ) ) {
+				config[prop] = defaults[prop];
+			}
+		}
+    } else {
+		config = defaults;
+	}
 } catch(err) {
-    console.err("Could not open configuration file %s! %s", commander.config, err);
+    console.error("Could not open configuration file %s! %s", commander.config, err);
     process.exit(1);
 }
 
 serverConfig = /(([0-9\.]*|\[[0-9a-fA-F\:]*\]):)?([0-9]+)/.exec(config.listen);
 if (!serverConfig) {
-    console.err("Server cannot listen on '%s', invalid format.", config.listen);
+    console.error("Server cannot listen on '%s', invalid format.", config.listen);
     process.exit(1);
 }
 
