f9d34dadd818f9fdcaef9ad6911fa4f8b660fb2e
Sherah Smith <ssmith@wikimedia.org>
2014-12-02 12:57:29 -0800

    fix navbar padding on collapse
    Change-Id: I44460d629202b5b0880c7e9bf9e0fe2b9cf28b13

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -13,7 +13,7 @@ define( [
         	//make the nav menu fold out of view.
             $('#navContainer .navWrapper').toggleClass('hide');
             $('#showNavMenu').css('display', 'inline');
-            $('#dashApp').css('padding', '0 0 0 55px');
+            $('#dashApp').css('padding', '0 0 0 80px');
         };
 
         self.showNav = function(){
