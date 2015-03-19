6cf3beb78666e05b5f8f3ad8fbfd09fb352d77a5
Elliott Eggleston <ejegg@ejegg.com>
2015-03-18 19:31:02 -0700

    Use shared context in distance to goal
    Display an error message if no widget exists to provide totals.
    
    Change-Id: I84c84b6213fe28070e759215f6c2c7b54d20e292

--- src/components/widgets/distance-to-goal-chart/distance-to-goal-chart.js
+++ src/components/widgets/distance-to-goal-chart/distance-to-goal-chart.js
@@ -2,79 +2,33 @@ define( [
 	'knockout',
 	'text!components/widgets/distance-to-goal-chart/distance-to-goal-chart.html',
 	'c3',
-	'momentjs'
-], function( ko, template, c3, moment ){
-
+	'WidgetBase'
+], function( ko, template, c3, WidgetBase ){
 
 	function DistanceToGoalChartViewModel( params ){
 
 		var self = this;
-		self.goal = ko.observable('20,000,000');
-		self.dailyDataArray = [ 'Daily Total' ];
+		WidgetBase.call( this, params );
+		self.hasData = ko.observable( false );
 
-		self.loadData = function ( decemberData, timestamp ) {
-			var runningTotal = 0,
-				currentDate = new Date(),
-				timeFormat = 'dddd, MMMM Do YYYY, h:mm:ss a';
-			currentDate.setTime( timestamp );
-			self.displayDate( moment( currentDate ).format( timeFormat ) );
-			self.lastDataPoint.day = currentDate.getUTCDate();
-			self.lastDataPoint.hour = currentDate.getUTCHours();
-
-			for (var d = 1; d < 32; d++) {
-				self.dailyDataArray[d] = 0;
-				self.dailyCountArray[d] = 0;
-				if (!self.dayObj[d]) {
-					self.dayObj[d] = new Array(25);
-					self.dayObj[d][0] = 'Hourly Totals';
-					for (var h = 0; h < 24; h++) {
-						self.dayObj[d][h + 1] = { total: 0, count: 0 };
-						self.secondsByHourDonationData[(d - 1) * 24 + h + 1] = 0;
-					}
-				}
-			}
-
-			var dataCount = decemberData.length;
-			for (var i = 0; i < dataCount; i++ ) {
-
-				var el = decemberData[i],
-						day = el.day,
-						hour = el.hour,
-						total = el.usd_total;
-				self.dayObj[day][hour + 1] = { total: total, count: el.donations };
-
-				self.secondsByHourDonationData[(day - 1) * 24 + hour + 1] = el.usd_per_second;
-				runningTotal += total;
-				self.dailyDataArray[day] += total;
-				self.dailyCountArray[day] += el.donations;
-			}
-
-			//self.makeCharts();
-
-			self.raised(runningTotal);
-		};
-
-		// $.get('data/big-english', function(dtgData){
-
-		// 	console.log('dtgData', dtgData);
-
-		// 	self.dailyDataArray;
-
-
-		// });
-
-		self.title = ko.observable(params.title);
 		self.makeCharts = function() {
-			if ( self.dailyDataArray.length < 2 ) {
+			if ( params.sharedContext.dailyDataArray.length < 2 ) {
 				return;
 			}
-			self.goal = ko.observable(self.goal);
+			self.hasData( true );
 
-			self.updatedGoal = self.goal();
+			self.updatedGoal = params.sharedContext.goal();
 			self.neededArray = ['Needed'];
-			for(var d = 1; d < self.dailyDataArray.length; d++) {
-				self.updatedGoal = self.updatedGoal - self.dailyDataArray[d];
+			for(var d = 1; d < params.sharedContext.dailyDataArray.length; d++) {
+				self.updatedGoal = self.updatedGoal - params.sharedContext.dailyDataArray[d];
 				self.neededArray[d] = self.updatedGoal >= 0 ? self.updatedGoal : 0;
+			}
+
+			if ( self.distanceToGoalChart ) {
+				self.distanceToGoalChart.load( {
+					columns: [ self.neededArray ]
+				} );
+				return;
 			}
 
 			self.distanceToGoalChart = c3.generate({
@@ -114,9 +68,7 @@ define( [
 				}
 			});
 		};
-
-		//self.makeCharts();
+		self.subscribe( params.sharedContext, 'totalsChanged', self.makeCharts );
 	}
 	return { viewModel: DistanceToGoalChartViewModel, template: template };
-
 });
