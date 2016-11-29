1b19cda380ef754b4fb82a0621e1674592cfdcae
Elliott Eggleston <ejegg@ejegg.com>
2016-11-29 14:20:21 -0600

    Revert "Enable syslog on OSX"
    This reverts commit 13337423c90a45a3bb5dfb3ead74b86d6f673d30.
    
    Change-Id: Ic60dcde9054e7f059efc4ccade1b1ad226dd0a20

--- logger.js
+++ logger.js
@@ -1,18 +1,24 @@
-var LOG_DEBUG = 0,
+var hasSyslog = !!process.platform.match(/linux/),
+	LOG_DEBUG = 0,
 	LOG_INFO = 1,	
 	LOG_ERR = 2,
-	syslog = require( 'modern-syslog' ),
+	syslog,
 	constMap = [];
 
-/*jslint bitwise: true*/
-syslog.init( 'dash', syslog.LOG_PID | syslog.LOG_ODELAY, syslog.LOG_LOCAL0 );
-/*jslint bitwise: false*/
-constMap[LOG_DEBUG] = syslog.LOG_DEBUG;
-constMap[LOG_INFO] = syslog.LOG_INFO;
-constMap[LOG_ERR] = syslog.LOG_ERR;
+if ( hasSyslog ) {
+	syslog = require( 'modern-syslog' );
+	/*jslint bitwise: true*/
+	syslog.init( 'dash', syslog.LOG_PID | syslog.LOG_ODELAY, syslog.LOG_LOCAL0 );
+	/*jslint bitwise: false*/
+	constMap[LOG_DEBUG] = syslog.LOG_DEBUG;
+	constMap[LOG_INFO] = syslog.LOG_INFO;
+	constMap[LOG_ERR] = syslog.LOG_ERR;
+}
 
 function log( level, message ) {
-	syslog.log( constMap[level], message );
+	if ( hasSyslog ) {
+		syslog.log( constMap[level], message );
+	}
 	if ( level === LOG_ERR ) {
 		console.error( message );
 	} else {
